fce4ed59d56501f691e0779f11ccc3ed7f7c0e64
Hebx <lord.noctx@gmail.com>
2022-04-08 22:04:53 +0000

    Initializing, Generating the transaction API

---
+++ pages/api/makeTransaction.ts
@@ -0,0 +1,100 @@
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base'
+import {
+  clusterApiUrl,
+  Connection,
+  PublicKey,
+  Transaction,
+  SystemProgram,
+  LAMPORTS_PER_SOL,
+} from '@solana/web3.js'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { shopAddress } from '../../lib/addresses'
+import calculatePrice from '../../lib/calculatePrice'
+
+export type MakeTransactionInputData = {
+  account: string
+}
+
+export type MakeTransactionOutputData = {
+  transaction: string
+  message: string
+}
+
+type ErrorOutput = {
+  error: string
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<MakeTransactionOutputData | ErrorOutput>
+) {
+  try {
+    // we pass the selected item in the query calculate the expected  cost
+    const amount = calculatePrice(req.query)
+    if (amount.toNumber() === 0) {
+      res.status(400).json({ error: `Can't checkout with charge of 0` })
+      return
+    }
+    // we pass the reference to use in the query
+    const { reference } = req.query
+    if (!reference) {
+      res.status(400).json({ error: `Missing reference` })
+      return
+    }
+    // we pass the buyer's public key in JSON body
+    const { account } = req.body as MakeTransactionInputData
+    if (!account) {
+      res.status(400).json({ error: `Missing account` })
+      return
+    }
+    const buyerPublicKey = new PublicKey(account)
+    const shopPublicKey = shopAddress
+    const network = WalletAdapterNetwork.Devnet
+    const endpoint = clusterApiUrl(network)
+    const connection = new Connection(endpoint)
+
+    // get a recent blockhash to include in the transaction
+    const { blockhash } = await connection.getLatestBlockhash('finalized')
+    const transaction = new Transaction({
+      recentBlockhash: blockhash,
+      // the buyer pays the transaction fee
+      feePayer: buyerPublicKey,
+    })
+
+    // create the instruction to send SOL from the buyer to the shop
+    const transferInstruction = SystemProgram.transfer({
+      fromPubkey: buyerPublicKey,
+      lamports: amount.multipliedBy(LAMPORTS_PER_SOL).toNumber(),
+      toPubkey: shopPublicKey,
+    })
+
+    // add the ref to the instruction as a key
+    // transaction is returned when we query for the ref
+    transferInstruction.keys.push({
+      pubkey: new PublicKey(reference),
+      isSigner: false,
+      isWritable: false,
+    })
+
+    // add the instruction to the transaction
+    transaction.add(transferInstruction)
+
+    // serialize the transaction and convert to base64 to return it
+    const serializedTransaction = transaction.serialize({
+      // buyer sign the transaction after it's returned to them
+      requireAllSignatures: false,
+    })
+    const base64 = serializedTransaction.toString('base64')
+
+    // insert into a database: info and amount
+    //  return the serialized transaction
+    res.status(200).json({
+      transaction: base64,
+      message: 'Thanks for your order 🔥',
+    })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: `error creating the transaction` })
+    return
+  }
+}
---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import { useWallet } from '@solana/wallet-adapter-react'
+import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import Products from '../components/Products'
+import SiteHeading from '../components/SiteHeading'
+
+export default function HomePage() {
+  const { publicKey } = useWallet()
+  return (
+    <div className="m-auto flex max-w-4xl flex-col items-stretch gap-8 pt-24">
+      <SiteHeading>Cookies Inc</SiteHeading>
+
+      {/* Solana Wallet Connect Button */}
+      <div className="basis-1/4">
+        <WalletMultiButton className="!bg-gray-900 hover:scale-105" />
+      </div>
+      {/* Disable checking out without a connected wallet */}
+      <Products submitTarget="/checkout" enabled={publicKey !== null} />
+    </div>
+  )
+}
